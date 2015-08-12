d1a59b125501f9a4efb1c64943789315d0c700ed
kaczla <kaczla@wp.pl>
2015-08-12 14:37:29 +0200

    Update
    Aktualizacja kamery

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,4 +1,6 @@
 #include "camera.hpp"
+#define GLM_FORCE_RADIANS
+#include <glm/gtx/transform.hpp>
 
 Camera::Camera(){
 
@@ -6,4 +8,46 @@ Camera::Camera(){
 
 Camera::~Camera(){
    
+}
+
+mat4 Camera::getModelToViewMatrix() const{
+   return lookAt( this->Position, this->Position + this->ViewDirection, this->Up );
+}
+
+void Camera::MouseUpdate( const vec2 &Mouse ){
+   /*
+   this->ViewDirection = mat3( rotate( Mouse.x * this->RotationSpeed, this->Up ) ) * this->ViewDirection;
+   this->RotationAround = cross( this->ViewDirection, this->Up );
+   this->ViewDirection = mat3( rotate( Mouse.y * this->RotationSpeed, this->RotationAround ) ) * this->ViewDirection;
+   */
+   //faster:
+   this->RotationAround = cross( this->ViewDirection, this->Up );
+   this->Rotation = rotate( Mouse.x * this->RotationSpeed, this->Up ) * rotate( Mouse.y * this->RotationSpeed, this->RotationAround );
+   this->ViewDirection = mat3( this->Rotation ) * this->ViewDirection;
+}
+
+void Camera::MoveUp(){
+   this->Position += this->MovementSpeed * this->Up;
+}
+
+void Camera::MoveDown(){
+   this->Position -= this->MovementSpeed * this->Up;
+}
+
+void Camera::MoveForward(){
+   this->Position += this->MovementSpeed * this->ViewDirection;
+}
+
+void Camera::MoveBackward(){
+   this->Position -= this->MovementSpeed * this->ViewDirection;
+}
+
+void Camera::MoveLeft(){
+   this->MovementDirection = cross( this->ViewDirection, this->Up );
+   this->Position -= this->MovementSpeed * this->MovementDirection;
+}
+
+void Camera::MoveRight(){
+   this->MovementDirection = cross( this->ViewDirection, this->Up );
+   this->Position += this->MovementSpeed * this->MovementDirection;
 }
--- src/camera.hpp
+++ src/camera.hpp
@@ -1,6 +1,5 @@
 #ifndef camera_hpp
 #define camera_hpp
-#include <iostream>
 #include <glm/glm.hpp>
 
 using namespace glm;
@@ -9,10 +8,25 @@ class Camera{
 public:
    Camera();
    ~Camera();
+   mat4 getModelToViewMatrix() const;
+   void MouseUpdate( const vec2 &Mouse );
+   void MoveUp();
+   void MoveDown();
+   void MoveForward();
+   void MoveBackward();
+   void MoveLeft();
+   void MoveRight();
 private:
-   vec3 VectorEye = vec3( 1.0f, 1.0f, 1.0f );
-   vec3 VectorCenter = vec3( 0.0f, 0.0f, 0.0f );
-   vec3 VectorUp = vec3( 0.0f, 1.0f, 0.0f );
+   vec3 Position = vec3( 1.0f, 1.0f, 1.0f );
+   vec3 ViewDirection = vec3( 0.0f, 0.0f, 0.0f );
+   vec3 Up = vec3( 0.0f, 1.0f, 0.0f );
+   //Rotation:
+   float RotationSpeed = 1.0f;
+   vec3 RotationAround;
+   mat4 Rotation;
+   //Movement:
+   float MovementSpeed = 0.5f;
+   vec3 MovementDirection;
 };
 
 #endif

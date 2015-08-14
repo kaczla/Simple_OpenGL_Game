85383f651d8be83f8245ab2ae64926a17e1c2b4a
kaczla <kaczla@wp.pl>
2015-08-14 17:36:44 +0200

    Add imgloader

---
+++ src/imgloader.hpp
@@ -0,0 +1,9 @@
+#ifndef imgloader_hpp
+#define imgloader_hpp
+#include <GL/glew.h>
+
+bool LoadImg( const char *img_path_file, GLuint &image );
+
+GLuint LoadImg( const char *img_path_file );
+
+#endif

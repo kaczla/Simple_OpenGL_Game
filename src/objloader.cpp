b7e285eb8f05197c547e76d687918ba0ce30db2e
kaczla <kaczla@wp.pl>
2015-08-21 15:09:15 +0200

    Update objloader
    Ładowanie obiektów przy pomocy biblioteki Assimp
    Tworzenie indeksów wierchołków VBO dla VAO

--- src/objloader.cpp
+++ src/objloader.cpp
@@ -2,7 +2,12 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <map>
+#include <cstring>
 #include <SDL2/SDL.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 bool LoadOBJ( const char* obj_path_file,
    std::vector <glm::vec3> &vertices,
@@ -146,5 +151,111 @@ bool LoadOBJ( const char* obj_path_file,
    }
    SDL_Log( "vertex:%u   uv:%u   normal:%u\n", vertices.size(), uvs.size(), normals.size() );
    SDL_Log( "Loaded OBJ: %s\n", obj_path_file );
+   return true;
+}
+
+struct Packe{
+   glm::vec3 position;
+   glm::vec2 uv;
+   glm::vec3 normal;
+   bool operator<(const Packe that) const{
+      return memcmp( (void*)this, (void*)&that, sizeof( Packe ) ) > 0;
+   };
+};
+
+bool GetSimilarVertexIndex(
+   Packe &packed,
+   std::map <Packe, GLuint> &VertexToOutIndex,
+   GLuint &result
+){
+   std::map <Packe, GLuint>::iterator it = VertexToOutIndex.find( packed );
+   if ( it == VertexToOutIndex.end() ){
+      return false;
+   }
+   else{
+      result = it->second;
+      return true;
+   }
+}
+
+void IndexVBO( std::vector <glm::vec3> &in_vertices,
+   std::vector <glm::vec2> &in_uvs,
+   std::vector <glm::vec3> &in_normals,
+   std::vector <GLuint> &out_indices,
+   std::vector <glm::vec3> &out_vertices,
+   std::vector <glm::vec2> &out_uvs,
+   std::vector <glm::vec3> &out_normals
+){
+   std::map <Packe, GLuint> VertexToOutIndex;
+   unsigned int i = 0;
+   GLuint index, newindex;
+   for ( i = 0; i < in_vertices.size(); ++i ){
+      Packe packed = {
+         in_vertices[i],
+         in_uvs[i],
+         in_normals[i]
+      };
+      bool found = GetSimilarVertexIndex( packed, VertexToOutIndex, index );
+      if ( found ){
+         out_indices.push_back( index );
+      }
+      else{
+         out_vertices.push_back( in_vertices[i] );
+         out_uvs.push_back( in_uvs[i] );
+         out_normals.push_back( in_normals[i] );
+         newindex = (GLuint)out_vertices.size() - 1;
+         out_indices.push_back( newindex );
+         VertexToOutIndex[packed] = newindex;
+      }
+   }
+}
+
+bool LoadAssimp( const char *path_file,
+   std::vector <glm::vec3> &vertices,
+   std::vector <glm::vec2> &uvs,
+   std::vector <glm::vec3> &normals,
+   std::vector <GLuint> &indices
+){
+   vertices.clear();
+   uvs.clear();
+   normals.clear();
+   indices.clear();
+   SDL_Log( "Loading file: %s\n", path_file );
+   Assimp::Importer importer;
+   const aiScene* scene = importer.ReadFile( path_file, 0 );
+   if( !scene ){
+      SDL_Log( "importer.ReadFile: %s\n", importer.GetErrorString() );
+      return false;
+   }
+   const aiMesh* mesh = scene->mMeshes[0];
+   unsigned int i = 0;
+   aiVector3D tmp;
+   //vertices:
+   vertices.reserve( mesh->mNumVertices );
+   for( i = 0; i < mesh->mNumVertices; ++i ){
+      tmp = mesh->mVertices[i];
+      vertices.push_back( glm::vec3( tmp.x, tmp.y, tmp.z ) );
+   }
+   //uvs:
+   uvs.reserve( mesh->mNumVertices );
+   for( i = 0; i < mesh->mNumVertices; ++i ){
+      tmp = mesh->mTextureCoords[0][i];
+      uvs.push_back( glm::vec2( tmp.x, tmp.y ) );
+   }
+   //normals:
+   normals.reserve( mesh->mNumVertices );
+   for( i = 0; i < mesh->mNumVertices; ++i ){
+      tmp = mesh->mNormals[i];
+      normals.push_back( glm::vec3( tmp.x, tmp.y, tmp.z ) );
+   }
+   //indices:
+   indices.reserve( 3*mesh->mNumFaces );
+   for( i = 0; i < mesh->mNumFaces; ++i ){
+      indices.push_back( mesh->mFaces[i].mIndices[0] );
+      indices.push_back( mesh->mFaces[i].mIndices[1] );
+      indices.push_back( mesh->mFaces[i].mIndices[2] );
+   }
+   SDL_Log( "vertex:%u   uv:%u   normal:%u   indices:%u\n", vertices.size(), uvs.size(), normals.size(), indices.size() );
+   SDL_Log( "Loaded file: %s\n", path_file );
    return true;
 }
--- src/objloader.hpp
+++ src/objloader.hpp
@@ -9,4 +9,20 @@ bool LoadOBJ( const char *obj_path_file,
    std::vector <glm::vec3> &normals
 );
 
+bool LoadAssimp( const char *path_file,
+   std::vector <glm::vec3> &vertices,
+   std::vector <glm::vec2> &uvs,
+   std::vector <glm::vec3> &normals,
+   std::vector <GLuint> &indices
+);
+
+void IndexVBO( std::vector <glm::vec3> &in_vertices,
+   std::vector <glm::vec2> &in_uvs,
+   std::vector <glm::vec3> &in_normals,
+   std::vector <GLuint> &out_indices,
+   std::vector <glm::vec3> &out_vertices,
+   std::vector <glm::vec2> &out_uvs,
+   std::vector <glm::vec3> &out_normals
+);
+
 #endif
